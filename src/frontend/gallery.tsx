d64cd46db756327c3447cfca3260a27f2f714779
Toshit Chawda <r58playz@gmail.com>
2025-06-09 15:12:40 -0700

    stupidly overcomplicated screenshot transform system

--- src/frontend/gallery.tsx
+++ src/frontend/gallery.tsx
@@ -9,8 +9,114 @@ import { BackIcon } from "../ui/Icon";
 import { Button } from "../ui/Button";
 import { Card } from "../ui/Card";
 import { TextInput } from "../ui/Input";
+import { fetch } from "../epoxy";
 
-const GalleryProject: Component<{ project: ProjectGallery }, { img: HTMLImageElement | string, upvoted: boolean, upvotes: number }> = function(cx) {
+let parser = new DOMParser();
+
+async function fetchImgur(url: URL): Promise<URL> {
+	let html = parser.parseFromString(await fetch(url.toString()).then(r => r.text()), "text/html");
+
+	let src = (html.querySelector("meta[name='twitter:image']") as HTMLMetaElement).content;
+	let srcUrl = new URL(src);
+	srcUrl.search = "";
+
+	return srcUrl;
+}
+
+async function fetchIbb(url: URL): Promise<URL> {
+	let html = parser.parseFromString(await fetch(url.toString()).then(r => r.text()), "text/html");
+
+	let src = (html.querySelector(".image-viewer-container img") as HTMLImageElement).src;
+	let srcUrl = new URL(src);
+	srcUrl.search = "";
+
+	return srcUrl;
+}
+
+async function fetchPostImg(url: URL): Promise<URL> {
+	let html = parser.parseFromString(await fetch(url.toString()).then(r => r.text()), "text/html");
+
+	let src = (html.querySelector("#main-image") as HTMLImageElement).src;
+	let srcUrl = new URL(src);
+	srcUrl.search = "";
+
+	return srcUrl;
+}
+
+async function fetchDrive(url: URL, set: (str: string) => void, useVideo: () => void): Promise<string> {
+	let id: string;
+	if (url.pathname.toLowerCase().startsWith("uc")) {
+		id = url.searchParams.get("id") || "";
+	} else {
+		let path = url.pathname.split("/");
+		id = path[path.findIndex(x => x === "d") + 1] || "";
+	}
+
+	let raw = `https://drive.usercontent.google.com/download?id=${id}`;
+	set(raw);
+	let res = await fetch(raw)
+	let blob = await res.blob();
+
+	if (new URL(res.url).hostname.toLowerCase() === "accounts.google.com") {
+		throw `Content is not accessible`;
+	} else if (blob.type.startsWith("video")) {
+		useVideo();
+	} else if (!blob.type.startsWith("image")) {
+		throw `Content type "${blob.type}" unsupported`;
+	}
+
+	return await new Promise((res, rej) => {
+		let reader = new FileReader();
+
+		reader.onload = () => {
+			res(reader.result as string);
+		};
+		reader.onerror = () => {
+			rej(reader.error);
+		}
+
+		reader.readAsDataURL(blob);
+	});
+}
+
+async function preprocessScreenshot(urlStr: string, set: (str: string) => void, useVideo: () => void): Promise<string> {
+	let url;
+	try {
+		url = new URL(urlStr);
+	} catch {
+		throw "Invalid URL";
+	}
+
+	if (!["http:", "https:"].includes(url.protocol.toLowerCase()))
+		throw `Invalid URL protocol: "${url.protocol}"`;
+
+	try {
+		let host = url.host.toLowerCase();
+		if (host === "imgur.com")
+			url = await fetchImgur(url);
+		else if (host === "ibb.co")
+			url = await fetchIbb(url);
+		else if (host === "postimg.cc")
+			url = await fetchPostImg(url);
+		else if (host === "github.com" && (!url.search.toLowerCase().includes("raw") || !url.pathname.toLowerCase().includes("raw")))
+			url.searchParams.set("raw", "true");
+		else if (host === "drive.google.com") {
+			return await fetchDrive(url, set, useVideo);
+		}
+	} catch (err) {
+		console.warn("transform failed for", urlStr, err);
+		throw `Failed to apply host-specific transform for "${url.host}"${typeof err === "string" ? `: ${err}` : ""}`;
+	}
+
+	return url.toString();
+}
+
+const GalleryProject: Component<{ project: ProjectGallery }, {
+	img: HTMLImageElement | HTMLVideoElement | string,
+	transformed: string,
+	upvoted: boolean,
+	upvotes: number
+}> = function(cx) {
 	cx.css = `
 		:scope :global(.Ui-card) {
 			height: 100%;
@@ -40,8 +146,11 @@ const GalleryProject: Component<{ project: ProjectGallery }, { img: HTMLImageEle
 			padding: 1em;
 
 			display: flex;
+			flex-direction: column;
+			gap: 0.5em;
 			align-items: center;
 			justify-content: center;
+			text-align: center;
 		}
 
 		.chips {
@@ -85,29 +194,49 @@ const GalleryProject: Component<{ project: ProjectGallery }, { img: HTMLImageEle
 	`;
 
 	this.img = "Loading";
+	this.transformed = this.project.screenshot;
 	this.upvoted = this.project.userUpvoted;
 	this.upvotes = this.project.upvoteCount;
 
 	cx.mount = async () => {
-		let img = new Image();
-		let promise = new Promise((res, rej) => {
-			img.addEventListener("load", res);
-			img.addEventListener("error", rej);
-		});
-
-		img.classList.add("img");
-
 		if (!this.project.screenshot) {
 			this.img = "No screenshot";
 			return;
 		}
 
-		img.src = this.project.screenshot;
+		let isVideo = false;
+
+		try {
+			this.transformed = await preprocessScreenshot(this.transformed, x => this.transformed = x, () => isVideo = true);
+		} catch (err) {
+			if (typeof err === "string") {
+				this.img = err;
+				return;
+			}
+		}
+
+		let embed = isVideo ? document.createElement("video") : new Image();
+		let promise = isVideo ? Promise.resolve() : new Promise((res, rej) => {
+			embed.addEventListener("load", res);
+			embed.addEventListener("error", rej);
+		});
+
+		embed.src = this.transformed;
+		embed.classList.add("img");
+		if (this.transformed !== this.project.screenshot) {
+			console.debug("transformed", this.project.screenshot, "->", this.transformed.startsWith("data") ? "data url" : this.transformed);
+			embed.setAttribute("data-transform", this.project.screenshot);
+		}
+
+		if (embed instanceof HTMLVideoElement) {
+			embed.controls = true;
+		}
 
 		try {
 			await promise;
-			this.img = img;
+			this.img = embed;
 		} catch {
+			console.warn("failed to load", this.transformed);
 			this.img = "Failed to load";
 		}
 	};
@@ -133,7 +262,21 @@ const GalleryProject: Component<{ project: ProjectGallery }, { img: HTMLImageEle
 						{this.project.playableUrl ? <a href={this.project.playableUrl} target="_blank">Demo</a> : null}
 						<span on:click={() => router.navigate("/reviews/" + this.project.projectID + "/gallery")} class="upvote">Reviews</span>
 					</div>
-					{use(this.img).map(x => typeof x === "string" ? <div class="img text" src={this.project.screenshot}>{x}</div> : x)}
+					{use(this.img).map(x => typeof x === "string" ? (
+						<div class="img text" data-url={this.project.screenshot} data-transform={this.transformed}>
+							{x}
+							{this.project.screenshot ? (
+								<div class="chips">
+									<a class="pink" href={this.project.screenshot} target="_blank">Open screenshot manually</a>
+								</div>
+							) : null}
+							{this.project.screenshot !== this.transformed ? (
+								<div class="chips">
+									<a class="pink" href={this.transformed} target="_blank">Open transformed screenshot manually</a>
+								</div>
+							) : null}
+						</div>
+					) : x)}
 					{this.project.description}
 				</div>
 			</Card>
